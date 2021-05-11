93740584feebccb8e0aa4f95f2412c76a19fdb06
neondude <siddharth.nagrajann@gmail.com>
2021-05-11 16:22:47 +0530

    update quick time format to 24 hrs

--- src/routes/Day.js
+++ src/routes/Day.js
@@ -191,7 +191,7 @@ class Day extends React.Component {
     const { year, month, day } = this.props
     const insertAt = (str, sub, pos) =>
       `${str.slice(0, pos)}${sub}${str.slice(pos)}`
-    const newText = insertAt(text, format(new Date(), "h:mma "), cursorIndex)
+    const newText = insertAt(text, format(new Date(), "HH:mm "), cursorIndex)
     this.setState({
       text: newText,
     })
